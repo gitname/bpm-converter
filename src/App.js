7135410cb89c8de4f60723286060ef101c565ca7
Eric Cavanna <almightyeric@gmail.com>
2017-12-13 15:54:57 -0800

    Lay out the UI elements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Container, Form, Header, Icon, Input, Segment } from 'semantic-ui-react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Icon name="check circle" size="massive"></Icon>
-      </div>
+      <Segment basic>
+        <Container>
+
+          <Header as="h1" textAlign="center">
+            <Icon name="music" color="teal"/>
+            <Header.Content>
+              BPM Converter
+            </Header.Content>
+            <Icon name="clock" color="purple"/>
+          </Header>
+
+          <p>
+            Enter the <strong>beats per minute</strong> or the <strong>milliseconds</strong> between each beat.
+            The other field will update in real-time to show an equivalent value, which you can
+            then <strong>copy</strong> to your clipboard.
+          </p>
+
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Field
+                action={{color: "teal", labelPosition: "right", icon: "copy", content: "Copy"}}
+                id="form-input-control-bpm"
+                control={Input}
+                label="Beats per minute"
+                placeholder="Beats per minute"
+                size="huge"
+              />
+              <Form.Field
+                action={{color: "purple", labelPosition: "right", icon: "copy", content: "Copy"}}
+                id="form-input-control-ms"
+                control={Input}
+                label="Milliseconds between each beat"
+                placeholder="Milliseconds between each beat"
+                size="huge"
+              />
+            </Form.Group>
+          </Form>
+
+        </Container>
+      </Segment>
     );
   }
 }
