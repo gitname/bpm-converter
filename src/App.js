4196613c3d86c82c8c04b5d6d4183131051ba224
Eric Cavanna <almightyeric@gmail.com>
2017-12-13 16:28:51 -0800

    Implement beats-per-minute to/from milliseconds-per-beat conversion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
-import { Container, Form, Header, Icon, Input, Segment } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Container, Form, Header, Icon, Input, Segment} from 'semantic-ui-react';
 
 import './App.css';
 
+const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const MILLISECONDS_PER_MINUTE = MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const initialBeatsPerMinute = 72;
+
+    this.state = {
+      beatsPerMinute: initialBeatsPerMinute,
+      millisecondsPerBeat: App.calculateMillisecondsPerBeat(initialBeatsPerMinute)
+    };
+  }
+
+  // TODO: Consider rounding the result to the nearest integer, and displaying a label that says whether we rounded it.
+
+  static calculateMillisecondsPerBeat(beatsPerMinute) {
+    return MILLISECONDS_PER_MINUTE / beatsPerMinute;
+  }
+
+  static calculateBeatsPerMinute(millisecondsPerBeat) {
+    return MILLISECONDS_PER_MINUTE / millisecondsPerBeat;
+  }
+
+  // TODO: Validate input data (e.g. accept positive integers only)
+
+  onBpmChange(event, data) {
+    this.setState({
+      beatsPerMinute: data.value,
+      millisecondsPerBeat: App.calculateMillisecondsPerBeat(data.value)
+    });
+  }
+
+  onMspbChange(event, data) {
+    this.setState({
+      beatsPerMinute: App.calculateBeatsPerMinute(data.value),
+      millisecondsPerBeat: data.value
+    });
+  }
+
   render() {
     return (
       <Segment basic>
@@ -18,7 +59,7 @@ class App extends Component {
           </Header>
 
           <p>
-            Enter the <strong>beats per minute</strong> or the <strong>milliseconds</strong> between each beat.
+            Enter the <strong>beats per minute</strong> or the <strong>milliseconds per beat</strong>.
             The other field will update in real-time to show an equivalent value, which you can
             then <strong>copy</strong> to your clipboard.
           </p>
@@ -27,19 +68,21 @@ class App extends Component {
             <Form.Group widths="equal">
               <Form.Field
                 action={{color: "teal", labelPosition: "right", icon: "copy", content: "Copy"}}
-                id="form-input-control-bpm"
                 control={Input}
                 label="Beats per minute"
+                onChange={this.onBpmChange.bind(this)}
                 placeholder="Beats per minute"
                 size="huge"
+                value={this.state.beatsPerMinute}
               />
               <Form.Field
                 action={{color: "purple", labelPosition: "right", icon: "copy", content: "Copy"}}
-                id="form-input-control-ms"
                 control={Input}
-                label="Milliseconds between each beat"
-                placeholder="Milliseconds between each beat"
+                label="Milliseconds per beat"
+                onChange={this.onMspbChange.bind(this)}
+                placeholder="Milliseconds per beat"
                 size="huge"
+                value={this.state.millisecondsPerBeat}
               />
             </Form.Group>
           </Form>
