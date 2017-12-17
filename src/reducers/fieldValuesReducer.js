f07c6ff77bd23e2e649fe012e7c943e7857ed4c1
Eric Cavanna <almightyeric@gmail.com>
2017-12-17 12:12:59 -0800

    Round results to nearest hundredth and prevent Infinity results

--- src/reducers/fieldValuesReducer.js
+++ src/reducers/fieldValuesReducer.js
@@ -1,22 +1,67 @@
-// A regular expression representing a floating point number in one of several formats.
+// A regular expression representing a floating-point number in one of several formats.
 const floatRegExp = /(?:^\d+$)|(?:^\.\d+$)|(?:^\d+\.$)|(?:^\d+\.\d+$)/;
 
 const MILLISECONDS_PER_MINUTE = 60 * 1000;
 
 const initialTempoStr = "120";
 
+/**
+ * Rounds the incoming floating-point number to the specified number of decimal places, then trims off any trailing 0s.
+ * Then, if a trailing period exists, trims that off also. Returns the resulting floating-point number.
+ *
+ * If the incoming floating-point number is 1e+21 or greater, returns null.
+ *
+ * @param inputFloat
+ * @param numDecimalPlaces
+ * @return {*}
+ * @private
+ */
+export const _roundAndTrimFloat = (inputFloat, numDecimalPlaces) => {
+  return (inputFloat < 1e+21) ? parseFloat(inputFloat.toFixed(numDecimalPlaces)) : null;
+};
+
+/**
+ * Returns a tempo string equivalent to the beat duration string passed in. If the beat duration string is an empty string,
+ * a period, or--when parsed as a float--is equal to 0; returns an empty string.
+ *
+ * @param beatDurationStr
+ * @return {string}
+ * @private
+ */
 export const _convertBeatDurationStrIntoTempoStr = (beatDurationStr) => {
-  return (beatDurationStr === "" || beatDurationStr === ".") ? "" : (MILLISECONDS_PER_MINUTE / beatDurationStr).toString();
+  return (beatDurationStr === "" || beatDurationStr === "." || parseFloat(beatDurationStr) === 0) ? "" : _roundAndTrimFloat((MILLISECONDS_PER_MINUTE / parseFloat(beatDurationStr)), 2).toString();
 };
 
+/**
+ * Returns a beat duration string equivalent to the tempo string passed in. If the tempo string is an empty string,
+ * a period, or--when parsed as a float--is equal to 0; returns an empty string.
+ *
+ * @param tempoStr
+ * @return {string}
+ * @private
+ */
 export const _convertTempoStrIntoBeatDurationStr = (tempoStr) => {
-  return (tempoStr === "" || tempoStr === ".") ? "" : (MILLISECONDS_PER_MINUTE / tempoStr).toString()
+  return (tempoStr === "" || tempoStr === "." || parseFloat(tempoStr) === 0) ? "" : _roundAndTrimFloat((MILLISECONDS_PER_MINUTE / parseFloat(tempoStr)), 2).toString();
 };
 
+/**
+ * Returns true if the string passed in is an empty string, a period, or a floating-point number in one of several formats.
+ *
+ * @param tempoStr
+ * @return {boolean}
+ * @private
+ */
 export const _validateTempoStr = (tempoStr) => {
   return (tempoStr === "" || tempoStr === "." || floatRegExp.test(tempoStr));
 };
 
+/**
+ * Returns true if the string passed in is an empty string, a period, or a floating-point number in one of several formats.
+ *
+ * @param beatDurationStr
+ * @return {boolean}
+ * @private
+ */
 export const _validateBeatDurationStr = (beatDurationStr) => {
   return (beatDurationStr === "" || beatDurationStr === "." || floatRegExp.test(beatDurationStr));
 };
--- src/reducers/test/fieldValuesReducer.test.js
+++ src/reducers/test/fieldValuesReducer.test.js
@@ -1,9 +1,38 @@
 import {
+  _roundAndTrimFloat,
   _convertBeatDurationStrIntoTempoStr,
   _convertTempoStrIntoBeatDurationStr,
   _validateTempoStr,
   _validateBeatDurationStr
 } from '../fieldValuesReducer';
+
+describe('_roundAndTrimFloat', function () {
+
+  it('converts 1e+21 to null', () => {
+    expect(_roundAndTrimFloat(1e+21, 2)).toBeNull();
+  });
+
+  it('considers only the immediately-following decimal place when rounding (does not "trickle-round")', () => {
+    expect(_roundAndTrimFloat(12.3449, 3)).toBe(12.345);
+    expect(_roundAndTrimFloat(12.3449, 2)).toBe(12.34); // Not 12.35
+  });
+
+  it('rounds to a specified decimal place', () => {
+    expect(_roundAndTrimFloat(12.344, 2)).toBe(12.34);
+    expect(_roundAndTrimFloat(12.345, 2)).toBe(12.35);
+  });
+
+  it('eliminates trailing 0s', () => {
+    expect(_roundAndTrimFloat(12.30, 2)).toBe(12.3);
+    expect(_roundAndTrimFloat(12.300, 2)).toBe(12.3);
+  });
+
+  it('eliminates extraneous decimal point', () => {
+    expect(_roundAndTrimFloat(12.0, 2)).toBe(12);
+    expect(_roundAndTrimFloat(12., 2)).toBe(12);
+  });
+
+});
 
 describe('_convertBeatDurationStrIntoTempoStr', function () {
 
@@ -13,6 +42,13 @@ describe('_convertBeatDurationStrIntoTempoStr', function () {
 
   it('converts a period into an empty string', () => {
     expect(_convertBeatDurationStrIntoTempoStr(".")).toEqual("");
+  });
+
+  it('converts various string forms of 0 into an empty string', () => {
+    expect(_convertTempoStrIntoBeatDurationStr("0")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr("0.")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr(".0")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr("0.0")).toEqual("");
   });
 
   it('converts "60" into "1000"', () => {
@@ -33,6 +69,13 @@ describe('_convertTempoStrIntoBeatDurationStr', function () {
 
   it('converts a period into an empty string', () => {
     expect(_convertTempoStrIntoBeatDurationStr(".")).toEqual("");
+  });
+
+  it('converts various string forms of 0 into an empty string', () => {
+    expect(_convertTempoStrIntoBeatDurationStr("0")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr("0.")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr(".0")).toEqual("");
+    expect(_convertTempoStrIntoBeatDurationStr("0.0")).toEqual("");
   });
 
   it('converts "60" into "1000"', () => {
